cd36bb35a8f1a2d726109630a9253d3250416a99
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-31 15:23:31 +0700

    Membuat class Budi

--- ParadigmaOOP2/ParadigmaOOP2.cpp
+++ ParadigmaOOP2/ParadigmaOOP2.cpp
@@ -34,3 +34,16 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+
+};
+
+
