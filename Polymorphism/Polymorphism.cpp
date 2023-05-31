e8e4a1d7a72c15f1946dd7bec7311739d911ed4c
muhfarrasm <muhammadfarrasm@gmail.com>
2023-05-31 19:13:12 +0700

    membuat class bernama joko

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -12,3 +12,13 @@ public:
 
 	}
 };
+
+class joko : public seseorang {
+public:
+	void pesan() {
+		cout << "Pesan dari Joko" << endl;
+	}
+	void cetak() {
+		cout << "Tulisan Joko" << endl;
+	}
+};
